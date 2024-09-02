64655389662c0f53c8db3c5796618e5c3675a8fe
Michael Gay <michael.mg.09a@gmail.com>
2024-09-02 12:30:43 +0200

    update

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+
+const button_menu = document.querySelector('.button_menu')
+const menu = document.querySelector('.nav_links')
+const container_menu = document.querySelector('.container_nav_links')
+
+button_menu.addEventListener('click', open_menu)
+
+function open_menu () {
+    menu.classList.add('show_nav_links')
+    container_menu.classList.add('show_container_nav_links')
+}
